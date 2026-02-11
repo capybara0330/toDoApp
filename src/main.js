84a3d26065ac75688624c784659608f2eb716acf
capybara0330 <lenashea03@gmail.com>
2026-02-10 23:45:28 -0500

    implement todo filtering

--- src/main.js
+++ src/main.js
@@ -12,8 +12,20 @@ function renderTodos(){
     const todoListElement = document.getElementById("todo-list");
     todoListElement.innerHTML = ""; //set empty for clean slate
 
+    let filteredTodos = [];
     for(let i = 0; i < todos.length; i++){
         const todo = todos[i];
+        if(filter === "all"){
+            filteredTodos.push(todo);
+        }else if(filter === "completed" && todo.completed){
+            filteredTodos.push(todo);
+        }else if(filter === "active" && !todo.completed){
+            filteredTodos.push(todo);
+        }
+    }
+
+    for(let i = 0; i < filteredTodos.length; i++){
+        const todo = filteredTodos[i];
 
         const todoItem = document.createElement("div");
         todoItem.classList.add("p-4", "todo-item");
@@ -44,6 +56,42 @@ function handleNewTodoKeyDown(event){
     }
 }
 
+function handleClickOnNavbar(event){
+    if(event.target.tagName === "A"){
+        const hrefValue = event.target.href;
+        const action = hrefValue.split("/").pop();
+        filter = action === "" ? "all" : action;
+        renderTodos();
+        renderTodoNavBar(hrefValue);
+    }
+}
+
+function renderTodoNavBar(href){
+    const elements = todoNav.children;
+    for(let i = 0; i < elements.length; i++){
+        const element = elements[i];
+        if(element.href === href){
+            element.classList.add(
+                "underline",
+                "underline-offset-4",
+                "decoration-rose-800",
+                "decoration-2",
+            );
+        }else{
+            element.classList.remove(
+                "underline",
+                "underline-offset-4",
+                "decoration-rose-800",
+                "decoration-2",
+            );
+        }
+    }
+}
+
 document.addEventListener("DOMContentLoaded", renderTodos);
+
 const newTodoInput = document.getElementById("new-todo");
 newTodoInput.addEventListener("keydown", handleNewTodoKeyDown);
+
+const todoNav = document.getElementById("todo-nav");
+todoNav.addEventListener("click", handleClickOnNavbar);
